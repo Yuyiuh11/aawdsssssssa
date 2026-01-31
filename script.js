aa563feb309bf56a8d2c391db0ccbfd729303081
Yuyiuh11 <yuyiuh123@gmail.com>
2026-01-31 00:42:51 -0300

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("https://ipapi.co/json/")
-    .then(res => {
-      if (!res.ok) throw new Error("HTTP error");
-      return res.json();
-    })
-    .then(data => {
-      document.getElementById("ip").textContent = data.ip;
-      document.getElementById("city").textContent = data.city;
-      document.getElementById("country").textContent = data.country_name;
-      document.getElementById("isp").textContent = data.org;
-    })
-    .catch(err => {
-      console.error(err);
-      document.body.innerHTML += "<p>Error al obtener datos</p>";
-    });
+  document.getElementById("browser").textContent = navigator.userAgent;
+  document.getElementById("os").textContent = navigator.platform;
+  document.getElementById("lang").textContent = navigator.language;
+  document.getElementById("res").textContent =
+    screen.width + " x " + screen.height;
 });
